7417ae1b2bfabd9d0ba47c2669c8dc241cdc8ef0
El Skwid (Don Morrison) <elskwid@gmail.com>
2012-09-23 13:32:52 -0700

    Update generator
    * Remove gem block - gems are included as 
    dependencies of the thincloud-test gem
    
    * Remove duplicative environment setup and call 
    mini_test:install

--- lib/generators/thincloud/test/test_generator.rb
+++ lib/generators/thincloud/test/test_generator.rb
@@ -5,44 +5,22 @@ module Thincloud
     class TestGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
-      desc "Generates the test harness."
+      desc "Generates thincloud test infrastructure."
       def test
-        gem_group :test do
-          gem "factory_girl_rails"
-          gem "guard"
-          gem "growl"
-          gem "minitest"
-          gem "guard-minitest"
-          gem "minitest-rails"
-          gem "minitest-rails-shoulda", "~> 0.1.0"
-          gem "rb-fsevent",             "~> 0.9.1"
-          gem "simplecov"
-          gem "mocha" # Must be after minitest
-        end
+        generate "mini_test:install"
 
-        application do
-          "config.generators { |g| g.test_framework :mini_test, spec: true, fixture: false }"
-        end
-
-        empty_directory "test/models"
-        empty_directory "test/controllers"
-        empty_directory "test/mailers"
-        empty_directory "test/helpers"
-        empty_directory "test/support"
-
-        run "touch test/{models,controllers,mailers,helpers,support}/.gitkeep"
-
+        remove_file "test/minitest_helper.rb"
         copy_file "minitest_helper.rb", "test/minitest_helper.rb"
 
         copy_file "test.rake", "lib/tasks/test.rake"
 
         copy_file "Guardfile"
 
+        create_file ".gitignore" unless File.exist?(".gitignore")
         append_file ".gitignore", "coverage"
 
         say_status "", ""
         say_status "success", "thincloud-test has finished."
-        say_status "", "Don't forget to run `bundle`"
       end
 
     end
