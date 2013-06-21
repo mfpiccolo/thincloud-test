c1e381576ae2efe8b7a8953a5a7cb1b9783b5385
Don Morrison <elskwid@gmail.com>
2013-06-21 12:43:45 -0700

    Don't need `File.join`

--- lib/thincloud/test/templates/test_helper.rb
+++ lib/thincloud/test/templates/test_helper.rb
@@ -2,4 +2,4 @@ require "thincloud/test"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.join("./test/support/**/*.rb")].sort.each { |f| require f }
+Dir["./test/support/**/*.rb"].sort.each { |f| require f }
